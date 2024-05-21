6d41f2c6167050915816f2bd89b0e40bda37be44
NeilMagon <neil.magon97@gmail.com>
2024-05-21 18:33:47 +0200

    feat: add movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        $movies = Movie::all();
+       
+        return view('home', ['movies' => $movies]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row row-cols-4">
+            @foreach ($movies as $movie)
+                <div class="col text-center">
+                    <h3>{{ $movie->title}}</h3>
+                    <h5>{{ $movie->original_title}}</h5>
+                    <h6>{{ $movie->nationality}}</h6>
+                    <p>{{ $movie->date}}</p>
+                    <p>{{ $movie->vote}}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
